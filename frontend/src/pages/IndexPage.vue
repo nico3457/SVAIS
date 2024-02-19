8e46c61aca217ede6d5f2edce6d979e117c3df7c
Nicolás Sousa González <125403467+nico3457@users.noreply.github.com>
2024-02-19 13:49:07 +0100

    Elipse (Círculo, por ahora)
    Se añadió la funcionalidad de pintar un circulo, a esperas de saber todos los detalles de la elipse. 
    
    Circulo de radio aleatorio, verde

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,52 +1,72 @@
 <template>
   <q-page>
-   <div id="map"></div>
+    <div id="map"></div>
   </q-page>
 </template>
 
 <script setup>
-  import {ref,onMounted} from 'vue'
+  import { ref, onMounted } from 'vue'
   import L from 'leaflet'
   import 'leaflet/dist/leaflet.css'
 
-  function initializeMapAndLocator(){
-    
-    var coordinate = [42.242306, -8.730914]
+  let currentMarker = null;
+  let currentEllipse = null;
+
+  function initializeMapAndLocator() {
+    const map = L.map('map').setView([42.242306, -8.730914], 14)
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      id: "streets-v12",
+      accessToken: "pk.eyJ1IjoiYm9id2F0Y2hlcngiLCJhIjoiY2xiMGwwZThrMWg3aTNwcW1mOGRucHh6bSJ9.kNHlmRqkRSxYNeipcKkJhw",
+    }).addTo(map);
+
     const boatIcon = L.icon({
       iconUrl: 'src/assets/cruise_colored-icon.png',
       iconSize: [32, 32],
       iconAnchor: [16, 16],
     });
 
-    var map = L.map('map')
-              .setView(coordinate, 14);
+    const boats = [
+      { coordinates: [42.242306, -8.730914] },
+      { coordinates: [42.245, -8.735] } // Agrega más coordenadas si es necesario
+    ];
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      id: "streets-v12",
-      accessToken:"pk.eyJ1IjoiYm9id2F0Y2hlcngiLCJhIjoiY2xiMGwwZThrMWg3aTNwcW1mOGRucHh6bSJ9.kNHlmRqkRSxYNeipcKkJhw",
-    }).addTo(map);
- 
-    L.marker(coordinate, 
-    { 
-      icon: boatIcon 
-    })
-    .addTo(map)
-    .bindPopup("Latitude: " + coordinate[0] + "Longitude: " + coordinate[0]);
- 
+    boats.forEach(boat => {
+      const marker = L.marker(boat.coordinates, { icon: boatIcon }).addTo(map);
+      marker.bindPopup(`Latitude: ${boat.coordinates[0]}, Longitude: ${boat.coordinates[1]}`);
+      marker.on('click', () => {
+        if (currentEllipse) {
+          currentEllipse.remove();
+        }
+        
+        drawEllipse(map, boat.coordinates);
+      });
+    });
+
+    function drawEllipse(map, center) {
+      const radius=Math.random() * (0.5 - 0.01) + 0.01; // Ajusta el radio de la elipse (en kilómetros)
+      const numberOfSegments = 36; // Define la cantidad de segmentos para la elipse
+      currentEllipse = L.circle([center[0], center[1]], {
+        color: 'dark',
+        fillColor: '#7FFF00', // Color gris muy suave
+        fillOpacity: 0.4, // Opacidad de relleno
+        radius: radius * 1000, // Convierte el radio de kilómetros a metros
+        interactive: false // Evita que la elipse sea interactiva
+      }).addTo(map);
+    }
   }
- 
-  onMounted(()=>{
-    initializeMapAndLocator();  
-  
+
+  onMounted(() => {
+    initializeMapAndLocator();
   })
 </script>
 
 <style>
 #map {
   width: 100%;
-  height: 92vh; 
+  height: 92vh;
   z-index: 1;
   display: flex;
   justify-content: center;
