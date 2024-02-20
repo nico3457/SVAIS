90f178eafe93d60a066bbf6461da4f249fe123df
Nicolás Sousa González <125403467+nico3457@users.noreply.github.com>
2024-02-20 14:17:11 +0100

    Update routes.js

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -14,6 +14,14 @@ const routes = [
       { path: '', component: () => import('pages/LoginPage.vue') }
     ]
   },
+  {
+    path: '/historical',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/HistoricalPage.vue') }
+    ]
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
