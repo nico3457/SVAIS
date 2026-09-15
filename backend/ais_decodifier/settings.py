ed929ba2060600f1f3716934d04e48e610709f56
Javier Bilbao lima <jbilbao@b080a1fe-28d4-4094-b2f2-68f161f5be47.fritz.box>
2026-09-15 21:03:50 +0200

    Load SECRET_KEY from environment

--- backend/ais_decodifier/settings.py
+++ backend/ais_decodifier/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
+# Set the DJANGO_SECRET_KEY environment variable before running the server.
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
